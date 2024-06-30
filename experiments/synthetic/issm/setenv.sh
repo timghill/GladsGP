216cf5eb97aafee79919b192fb099a1ed02f7481
timghill <tha111@sfu.ca>
2024-06-30 12:22:24 -0400

    Requirements and regenerate inputs

--- experiments/synthetic/issm/setenv.sh
+++ experiments/synthetic/issm/setenv.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 module purge
 
-module load StdEnv/2023 petsc/3.20.0 matlab/2023b meta-farm/1.0.2
+module load StdEnv/2023 petsc/3.20.0 matlab/2023b meta-farm/1.0.2 mpi4py/3.1.4
+
+export PYENV=/home/tghill/SFU-code/GladsGP/pyenv
