5177d4278259d7a3fdf962c436ca8373d60836d3
Tim <tha111@sfu.ca>
2024-07-03 00:18:24 -0400

    Add farm to module list

--- experiments/greenland/issm/setenv.sh
+++ experiments/greenland/issm/setenv.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 module purge
 
-module load StdEnv/2023 petsc/3.20.0 matlab/2023b meta-farm/1.0.2
+module load StdEnv/2023 openmpi matlab metis parmetis imkl petsc python/3.11.5 mpi4py meta-farm/1.0.2
