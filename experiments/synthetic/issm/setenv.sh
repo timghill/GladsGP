fb8e3e6e0832972d871c5137927311c871fb75a0
Tim <tha111@sfu.ca>
2024-07-02 23:00:21 -0400

    Run full ensembles

--- experiments/synthetic/issm/setenv.sh
+++ experiments/synthetic/issm/setenv.sh
@@ -1,6 +1,1 @@
-#!/bin/bash
-module purge
-
-module load StdEnv/2023 petsc/3.20.0 matlab/2023b meta-farm/1.0.2 mpi4py/3.1.4
-
-export PYENV=/home/tghill/SFU-code/GladsGP/pyenv
+module load StdEnv/2023 openmpi matlab metis parmetis imkl petsc python/3.11.5 mpi4py
--- experiments/synthetic/issm/train/job_script.sh
+++ experiments/synthetic/issm/train/job_script.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
-#SBATCH --job-name="train"
-#SBATCH --time=0-24:00
+#SBATCH --job-name="synth-train"
+#SBATCH --time=00-24:00
 #SBATCH --mem=2G
 #SBATCH --account=def-gflowers
 #SBATCH --mail-user=tha111@sfu.ca
 #SBATCH --mail-type=FAIL,END
 
-# Time calculation
-# Each job: ~30 minutes for 2 years of simulation time
-
-# Don't change this line:
-
-source ../setenv.sh
+source ~/SFU-code/issmenv.sh
 task.run
