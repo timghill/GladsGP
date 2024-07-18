e118b04e703fbb1cc9bba8a9f4d0dff4cf733090
Tim <tha111@sfu.ca>
2024-07-18 13:12:23 -0400

    Update models

--- experiments/synthetic/analysis/submit_assess.sh
+++ experiments/synthetic/analysis/submit_assess.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name="assess"
-#SBATCH --time=0-8:00
+#SBATCH --time=0-16:00
 #SBATCH --mem=48G
 #SBATCH --account=def-gflowers
 #SBATCH --mail-user=tha111@sfu.ca
@@ -11,4 +11,4 @@
 source ../issm/setenv.sh
 source ~/SFU-code/GladsGP/pyenv/bin/activate
 
-python -u assess_all_models.py ../train_config.py ../test_config.py --npc {1..11} --nsim 512 256 128 64 32 16 -r
+python -u assess_all_models.py ../train_config.py ../test_config.py --npc {1..11} --nsim 16 32 64 128 256 512 -r
