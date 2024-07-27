5694a291ffb65e6fba45261f9a071fb7e8de9185
Tim <tha111@sfu.ca>
2024-07-27 11:27:16 -0400

    Fit models

--- experiments/greenland/analysis/submit_fits_big.sh
+++ experiments/greenland/analysis/submit_fits_big.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH --job-name="fits-big"
 #SBATCH --time=0-06:00
-#SBATCH --mem=64G
+#SBATCH --mem=48G
 #SBATCH --account=def-gflowers
 #SBATCH --mail-user=tha111@sfu.ca
 #SBATCH --mail-type=FAIL,END
 
-soure ../issm/setenv.sh
+source ../issm/setenv.sh
 source ~/SFU-code/GladsGP/pyenv/bin/activate
 
-python -u fit_all_models.py ../train_config.py --npc {1..11} --nsim 512 256 -r
+python -u fit_all_models.py ../train_config.py --npc {8..11} --nsim 256 -r
--- experiments/greenland/analysis/submit_fits_small.sh
+++ experiments/greenland/analysis/submit_fits_small.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH --job-name="fit-small"
 #SBATCH --time=0-03:00
-#SBATCH --mem=24G
+#SBATCH --mem=32G
 #SBATCH --account=def-gflowers
 #SBATCH --mail-user=tha111@sfu.ca
 #SBATCH --mail-type=FAIL,END
 
-soure ../issm/setenv.sh
+source ../issm/setenv.sh
 source ~/SFU-code/GladsGP/pyenv/bin/activate
 
-python -u fit_all_models.py ../train_config.py --npc {1..11} --nsim 128 64 32 16 -r
+python -u fit_all_models.py ../train_config.py --npc {1..11} --nsim 64 32 16 -r
