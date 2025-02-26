433bae2682fe55e6a53f1c7f383a36be2163fc24
Tim <tha111@sfu.ca>
2025-02-26 11:54:20 -0500

    Greenland scripts

--- experiments/greenland/analysis/test/job_script.sh
+++ experiments/greenland/analysis/test/job_script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name="fit-jobs"
-#SBATCH --time=0-01:00
-#SBATCH --mem=14G
+#SBATCH --time=0-06:00
+#SBATCH --mem=30G
 #SBATCH --account=def-gflowers
 #SBATCH --mail-user=tha111@sfu.ca
 #SBATCH --mail-type=FAIL,END,BEGIN
