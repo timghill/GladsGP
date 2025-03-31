47f444c2b6b80ffc2a448e49c57a726d6c25d22b
Tim Hill <tha111@sfu.ca>
2025-03-31 15:42:44 -0700

    Combined min/median criteria

--- experiments/greenland/analysis/negative_flot.py
+++ experiments/greenland/analysis/negative_flot.py
@@ -15,6 +15,7 @@ Y = Y.reshape((nx, nt, -1))
 ywinter = Y[:, 100]
 
 ymed = np.median(ywinter, axis=-1)
+ymin = np.min(ywinter, axis=-1)
 
 mesh = np.load('../issm/data/geom/IS_mesh.pkl', allow_pickle=True)
 
@@ -44,10 +45,10 @@ add_neg = 0
 # nticks_neg = logmin - logabsmin
 
 ylog_pos = np.nan*np.zeros(ymed.shape)
-ylog_pos[ymed>0] = np.log10(ymed[ymed>0])
+ylog_pos[ymin>0] = np.log10(ymin[ymin>0])
 
-ylog_neg = np.nan*np.zeros(ymed.shape)
-ylog_neg[ymed<0] = np.log10(-ymed[ymed<0])
+ylog_neg = np.nan*np.zeros(ymin.shape)
+ylog_neg[ymin<0] = np.log10(-ymin[ymin<0])
 
 # cticks = np.arange(-add_pos, 0.1)
 # cticklabels = [r'10$^{{{}}}$'.format(int(x)) for x in cticks]
@@ -69,7 +70,8 @@ cbar_neg = fig.colorbar(pc, cax=cax2, label=r'Min winter flotation fraction $f_{
 cax2.set_xticks(cticks, cticklabels)
 
 # ax.tripcolor(mtri, ylog_neg, cmap=cmocean.cm.rain)
-ax.tricontour(mtri, ymed, levels=[0], colors='black')
+ax.tricontour(mtri, ymin, levels=[-1./3.], colors='black', linestyles='solid')
+ax.tricontour(mtri, ymed, levels=[0], colors='gray', linestyles='solid')
 ax.spines[['left', 'right', 'top', 'bottom']].set_visible(False)
 ax.set_xticks([])
 ax.set_yticks([])
@@ -81,12 +83,6 @@ ax.set_ylim([-2510, -2485])
 # mock_cmap[:128] = 
 fig.savefig('figures/median_winter_flot_map_divlog.png', dpi=400)
 
-N_neg = len(ymed[ymed<0])
-N = ymed.size
-N_prop = N_neg/N
-print(f'{N_neg} ({N_prop:.3%}) negative nodes')
-
-print('Min flot frac:', np.min(ymed))
 
 fig,ax = plt.subplots()
 ax.hist(ymed, bins=50)
@@ -96,12 +92,28 @@ ax.set_xlabel('Min winter flotation fraction')
 ax.set_ylabel('Count')
 fig.savefig('figures/median_winter_flot_hist.png', dpi=400)
 
+
+print('Min flot frac:', np.min(ymed))
+
 ymed_element = np.mean(ymed[mesh['elements']-1], axis=1)
 print('ymed_element:', ymed_element.shape)
 
-A_neg = np.sum(mesh['area'][ymed_element<0])
-A_tot = np.sum(mesh['area'])
-A_prop = A_neg/A_tot
-print(f'Area: {A_neg:.3e}  ({A_prop:.3%})')
+ymin_element = np.mean(ymin[mesh['elements']-1], axis=1)
+print('ymin_element:', ymin_element.shape)
+
+A_combined = mesh['area'].copy()
+A_combined[ymed_element<0] = np.nan
+A_combined[ymin_element<-1./3.] = np.nan
+
+A_neg = np.nansum(A_combined)
+A_tot = np.nansum(mesh['area'])
+
+A_removed = A_tot - A_neg
+A_removed_prop = A_removed/A_tot
+
+N_neg = len(A_combined[np.isnan(A_combined)])
+N_neg_prop = N_neg / len(A_combined)
+print(f'Removed nodes: {N_neg}  ({N_neg_prop:.3%})')
+print(f'Removed area: {A_removed:.3e}  ({A_removed_prop:.3%})')
 
 # plt.show()
